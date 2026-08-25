128edb013e0ee83a62f3a619b11f5eb1435f4ea9
Chetan-Tiwari-2430 <chetantiwari2430@gmail.com>
2026-08-25 19:50:21 +0530

    25-Aug-2026

--- Task List/Slicing.py
+++ Task List/Slicing.py
@@ -1,12 +1,16 @@
 # Extract first 10, last 5, alternate elements.
 import numpy as np
+
 nums = np.arange(1,51)
 print("Original Array: ",nums)
+
 # First 10 Element
 ten_elements = nums[:10]
 print("First Ten elements: ",ten_elements)
+
 # Last 5 Elements
 five_last_elements = nums[-5:]
 print("Last Five Elements: ",five_last_elements)
+
 # Alternate Elements
 print("Alternate Elements: ",nums[::2])
