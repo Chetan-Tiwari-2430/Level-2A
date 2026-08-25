8f9e362aaf015e2d6985c171b0b171147f6675d2
Chetan-Tiwari-2430 <chetantiwari2430@gmail.com>
2026-08-25 19:49:06 +0530

    25-Aug-2026

---
+++ Task List/Indexing.py
@@ -0,0 +1,7 @@
+# Access first, last, and middle elements.
+# Use Indexing
+import numpy as np
+nums = np.array([1,2,2,3,3,4,4,5,6])
+print("First Element: ",nums[0])
+print("Middle Element: ",nums[nums.size//2])
+print("Last Element: ",nums[nums.size-1])
