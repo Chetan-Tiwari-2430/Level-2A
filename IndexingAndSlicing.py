f8fd17f604592b0c204fa562ab4b14b9c1cb7f16
Chetan-Tiwari-2430 <chetantiwari2430@gmail.com>
2026-09-01 14:38:19 +0530

    1-Sep-2026

--- IndexingAndSlicing.py
+++ IndexingAndSlicing.py
@@ -1,4 +1,4 @@
-import numpy as np
+ import numpy as np
 
 # Indexing and Slicing in the 1D Array
 # nums = np.array([1,2,3,4,5,6,7,8,9,10])
---
+++ Task List/Mathemetical.py
@@ -0,0 +1,11 @@
+import numpy as np
+nums1 = np.array([1,2,3,4,5])
+nums2 = np.array([6,7,8,9,10])
+sum_array = (nums1+nums2)
+multiply_array = nums1*nums2
+subtract_array = nums1-nums2
+print("Array 1: ",nums1)
+print("Array 2: ",nums2)
+print("Array Sum: ",sum_array)
+print("Multiply Array: ",multiply_array)
+print("Subtract Array: ",subtract_array)
