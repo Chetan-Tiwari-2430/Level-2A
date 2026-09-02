5bae1fcb8225e2a3027fcc13932c36e332553c3c
Chetan Tiwari <chetantiwari2430@gmail.com>
2026-09-02 14:36:45 +0530

    "2-Sep-2026"

---
+++ Task List/StandardDeviation.py
@@ -0,0 +1,7 @@
+import numpy as np
+data = np.array([2,3,4,5,7,3,2,2])
+variance = np.var(data)
+standard_deviation = np.std(data)
+print("Data: ",data)
+print("Variance: ",variance)
+print("Standard Deviation: ",standard_deviation)
