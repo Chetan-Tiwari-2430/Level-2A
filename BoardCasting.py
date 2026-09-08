0938b3652a540c81125d03ec65d8e6931490c123
Chetan-Tiwari-2430 <chetantiwari2430@gmail.com>
2026-09-08 13:03:54 +0530

    8-Sep-2026

--- BoardCasting.py
+++ BoardCasting.py
@@ -1,1 +1,5 @@
 # Add bonus 5 marks to all of the students
+import numpy as np
+marks = np.array([20,30,40,5,78,90,98,95])
+updated_marks = np.where(marks <= 95,marks + 5,marks)
+print("Updated Marks",updated_marks)
