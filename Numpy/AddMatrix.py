178eb5674e097881cca41797cc4bcb20d9b5fe37
Chetan-Tiwari-2430 <chetantiwari2430@gmail.com>
2026-09-18 10:17:18 +0530

    18-Sep-2026

---
+++ Numpy/AddMatrix.py
@@ -0,0 +1,15 @@
+import numpy as np
+matrix1 = np.array([
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+])
+
+matrix2 = np.array([
+    [10,11,12],
+    [13,14,15],
+    [16,17,18]
+])
+
+sum = matrix1 + matrix2
+print("Sum of the 2 [3X3] matrixes: \n",sum)
