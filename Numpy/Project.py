c1904f88c37c10ddf50848e85bc8d33a45b504c9
Chetan-Tiwari-2430 <chetantiwari2430@gmail.com>
2026-09-18 15:27:50 +0530

    Added the passed Students count

--- Numpy/Project.py
+++ Numpy/Project.py
@@ -2,7 +2,20 @@
 import numpy as np
 
 def is_pass(nums):
-    is_student_pass = True
+    #is_pass_student = np.array([])
+    rows = nums.shape[0]
+    pass_list = np.zeros(rows)
+    for i in range(rows):
+        arr = nums[i,1:]
+        is_passed = np.where(arr >= 35,True,False)
+        if False in is_passed:
+            pass_list[i] = 0
+        else:
+            pass_list[i] = 1
+
+    return int(sum(pass_list))
+        
+        
 
 names = {
     1: "Aarav",
@@ -18,17 +31,19 @@ names = {
 }
 
 students_marks = np.array([
-    [1, 12, 45, 78, 23, 56],
-    [2, 91, 34, 67, 89, 10],
+    [1, 62, 45, 78, 94, 56],
+    [2, 91, 44, 67, 89, 50],
     [3, 43, 76, 21, 54, 88],
     [4, 65, 19, 92, 37, 41],
     [5, 28, 83, 49, 71, 15],
-    [6, 57, 32, 86, 24, 69],
-    [7, 11, 64, 38, 95, 47],
-    [8, 79, 26, 53, 18, 84],
+    [6, 57, 72, 86, 84, 69],
+    [7, 51, 64, 78, 95, 47],
+    [8, 79, 78, 53, 68, 84],
     [9, 35, 72, 14, 61, 97],
     [10, 48, 90, 31, 66, 22]])
 
 
 total_marks = np.sum(students_marks[:,1:],axis = 0)
 average_marks = total_marks // students_marks.shape[0]
+total_passed_students = is_pass(students_marks)
+print("Total Passes Student: ",total_passed_students)
